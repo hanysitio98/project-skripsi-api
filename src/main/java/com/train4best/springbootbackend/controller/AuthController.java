5ce67b46c5283c1f74b8f07a8461f376839d3cdc
Hany Juliana <64250079+hanysitio98@users.noreply.github.com>
2023-01-04 15:15:42 +0700

    update cors

--- src/main/java/com/train4best/springbootbackend/controller/AuthController.java
+++ src/main/java/com/train4best/springbootbackend/controller/AuthController.java
@@ -18,11 +18,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -51,6 +49,7 @@ public class AuthController {
         String token = jwtUtils.generateJwtToken(authentication);
         String refreshToken = jwtUtils.generateRefresJwtToken(authentication);
         UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+
         return ResponseEntity.ok()
                 .body(new JwtResponse(token, refreshToken, principal.getUsername(), principal.getEmail()));
     }
